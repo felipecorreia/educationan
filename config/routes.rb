766f755ea7dd69e0f4f3ad95f4ae810b98760b85
Felipe <felipecorrei@gmail.com>
2013-11-25 17:05:52 -0200

    Atualizando rotas 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Educationan::Application.routes.draw do
 
-  root 'welcome#index'
+match ':controller(/:action(/:id(.:format)))'
+
+
+
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
